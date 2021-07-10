e46eb2db89417e68f26070a93ecb06e23813d9ac
do hieu <hieuminh2102@gmail.com>
2021-07-10 17:55:39 +0700

    lesson section-18a

---
+++ back-end/lesson/section-18/shopping_cart/index.php
@@ -0,0 +1,24 @@
+<?php
+#dữ liệu
+require 'data/pages.php';
+require 'data/products.php';
+#thư viện hàm
+require 'lib/pages.php';
+require 'lib/data.php';
+require 'lib/template.php';
+?>
+
+<?php
+$mod = !empty($_GET['mod'])?$_GET['mod']:'home';
+$act = !empty($_GET['act'])?$_GET['act']:'main';
+$path = "module/{$mod}/{$act}.php";
+if(file_exists($path)){
+    require $path;
+}else{
+    require 'inc/404.php';
+}
+?>
+
+<?php
+get_footer();
+?>
